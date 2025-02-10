1715bc143dbd6ca07e173bf41219d0b49a09d882
arnavvashisht135 <arnav.vashisht@education.nsw.gov.au>
2025-02-10 09:20:26 +0000

    Upload commpleted extensions.py

---
+++ 1-Conditionals/extensions/extensions.py
@@ -0,0 +1,15 @@
+file = input('Name of file? ').strip().lower()
+if file == ('.gif'):
+    print('image/gif')
+elif file == ('.jpg', '.jpeg'):
+    print('image/jpeg')
+elif file == ('.png'):
+    print('image/png')
+elif file == ('.pdf'):
+    print('application/pdf')
+elif file == ('.txt'):
+    print('text/plain')
+elif file == ('.zip'):
+    print('application/zip')
+else:
+    print('N/A')
