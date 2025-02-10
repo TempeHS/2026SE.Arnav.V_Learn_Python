cfde2d88ce0108c3fa91fc27a844a4c708795bc2
arnavvashisht135 <arnav.vashisht@education.nsw.gov.au>
2025-02-10 08:17:39 +0000

    Upload completed deep.py

---
+++ 1-Conditionals/deep/deep.py
@@ -0,0 +1,5 @@
+answer = input('What is the awnser to the great question of life, the universe, and everything').strip().lower()
+if answer == "42" or answer == 'forty-two' or answer == 'forty two':
+    print('Yes')
+else:
+    print('No')
