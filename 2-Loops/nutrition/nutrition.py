6a498119962d328619b00e3239190f594f6be027
arnavvashisht135 <arnav.vashisht@education.nsw.gov.au>
2025-03-15 02:03:38 +0000

    Upload completed nutrition.py

---
+++ 2-Loops/nutrition/nutrition.py
@@ -0,0 +1,45 @@
+def main():
+    food = input("Enter a fruit or veggie: ").lower().strip()
+    if food == 'apple':
+        print('130')
+    elif food == 'banana':
+        print('110')
+    elif food == 'avacado':
+        print('50')
+    elif food == 'cantaloupe':
+        print('50')
+    elif food == 'grapefruit':
+        print('60')
+    elif food == 'grape':
+        print('90')
+    elif food == 'honeydewmelon':
+        print('50')
+    elif food == 'kiwi':
+        print('90')
+    elif food == 'lemon':
+        print('20')
+    elif food == 'lime':
+        print('20')
+    elif food == 'nectarine':
+        print('60')
+    elif food == 'orange':
+        print('80')
+    elif food == 'peach':
+        print('60')
+    elif food == 'pear':
+        print('100')
+    elif food == 'pineapple':
+        print('50')
+    elif food == 'plum':
+        print('70')
+    elif food == 'strawberry':
+        print('50')
+    elif food == 'sweet cherry':
+        print('100')
+    elif food == 'tangerine':
+        print('50')
+    elif food == 'watermelon':
+        print('80')
+    else:
+        print(' ')
+main()
